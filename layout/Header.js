221db4b4b46c13a82dad37af37d972b79b28d3ad
JongBok5057 <ljb0362@gmail.com>
2021-02-22 20:53:53 +0900

    Header 메뉴 추가

--- layout/Header.js
+++ layout/Header.js
@@ -1,7 +1,15 @@
 const Header = () => {
     return (
         <div>
-            header 입니다.
+            <div className="headerspace">
+                <div classname="navspace">
+                    <div>기능소개</div>
+                    <div>가격정책</div>
+                    <div>비대면 바우처</div>
+                    <div>블로그</div>
+                    <div>로그인</div>
+                </div>
+            </div>
         </div>
     )
 }
