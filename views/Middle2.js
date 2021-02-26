5f3d467878802d37a708bb7378978f5b27c2371b
JongBok5057 <ljb0362@gmail.com>
2021-02-26 18:55:09 +0900

    Middle2 영역 추가

--- views/Middle2.js
+++ views/Middle2.js
@@ -1,9 +1,426 @@
 import styled from 'styled-components';
 
+const Middle2Section = styled.div `
+    section {
+        background-color: white;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        width: 100%;
+        padding: 120px 32px;
+        box-sizing: border-box;
+    }
+`;
+
+const Middle2Area = styled.div `
+    div {
+        display: flex;
+        flex-direction: column;
+        -webkit-box-align: center;
+        align-items: center;
+        width: 100%;
+        margin-bottom: 60px;
+    }
+`;
+
+const H2 = styled.div `
+    h2 {
+        margin: 0px 0px 36px;
+        font-size: 46px;
+        line-height: 1.26;
+        white-space: pre-line;
+        text-align: center;
+        max-width: 100%;
+        color: rgb(12, 36, 59);
+        display: block;
+        margin-block-start: 0.83em;
+        margin-block-end: 0.83em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+    }
+`;
+
+const H3 = styled.div `
+    h3 {
+        margin: 0px;
+        font-weight: 400;
+        max-width: 100%;
+        color: rgb(62, 82, 101);
+        font-size: 14px;
+        line-height: 1.86;
+        white-space: pre-line;
+        text-align: center;
+        display: block;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+    }
+`;
+
+const Middle2Area2 = styled.div `
+    div {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        -webkit-box-align: center;
+        align-items: center;
+    }
+`;
+
+const Ul = styled.div `
+    ul {
+        width: 368px;
+        height: 40px;
+        border-radius: 8px;
+        margin-bottom: 30px;
+        padding: 4px;
+        background-color: rgba(139, 152, 167, 0.08);
+        display: flex;
+        -webkit-box-flex: 0;
+        flex-grow: 0;
+        font-size: 13px;
+        box-sizing: border-box;
+        opacity: 1;
+        box-shadow: rgb(46 46 51 / 2%) 0px 1px 2px 0px inset;
+        margin: 0;
+        list-style-type: disc;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        padding-inline-start: 40px;
+    }
+`;
+
+const Li = styled.div `
+    li {
+        margin-left: 0px;
+        height: 100%;
+        display: flex;
+        -webkit-box-flex: 1;
+        flex: 1 1 1px;
+        text-align: -webkit-match-parent;
+        font-size: 9px;
+        list-style: none;
+    }
+`;
+
+
+const Li23 = styled.div `
+    li {
+        height: 100%;
+        margin-left: 4px;
+        display: flex;
+        -webkit-box-flex: 1;
+        flex: 1 1 1px;
+        list-style: none;
+        font-size: 9px;
+    }
+`;
+
+const Button = styled.div `
+    button {
+        position: relative;
+        overflow: hidden;
+        color: rgb(29, 56, 83);
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        border-radius: 4px;
+        background-color: rgb(255, 255, 255);
+        display: flex;
+        font-size: 13px;
+        font-weight: 600;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        box-shadow: rgb(20 20 20 / 4%) 3px 6px 12px 0px, rgb(20 20 20 / 10%) 0px 1px 4px 0px;
+        border: none;
+        outline: none;
+        appearance: auto;
+        -webkit-writing-mode: horizontal-tb !important;
+        letter-spacing: normal;
+        word-spacing: normal;
+        text-transform: none;
+        text-indent: 0px;
+        text-shadow: none;
+        text-align: center;
+        box-sizing: border-box;
+        font: 400 13.3333px Arial;
+        content: "";
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: transparent;
+        transition: all 0.15s ease 0s;
+        pointer-events: none;
+        opacity: 0;
+        border-radius: 4px;
+    }
+`;
+
+const Button23 = styled.div `
+    button {
+        position: relative;
+        overflow: hidden;
+        color: rgb(139, 152, 167);
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+        border-radius: 4px;
+        display: flex;
+        font-size: 13px;
+        font-weight: 600;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        border: none;
+        outline: none;
+        background-color: transparent;
+        appearance: auto;
+        -webkit-writing-mode: horizontal-tb !important;
+        letter-spacing: normal;
+        word-spacing: normal;
+        text-transform: none;
+        text-indent: 0px;
+        text-shadow: none;
+        text-align: center;
+        box-sizing: border-box;
+        font: 400 13.3333px Arial;
+        content: "";
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: transparent;
+        transition: all 0.15s ease 0s;
+        pointer-events: none;
+        opacity: 0;
+        border-radius: 4px;
+    }
+`;
+
+const ImgArea = styled.div `
+    div {
+        padding: 32px 14px 24px;
+        height: 352px;
+        box-sizing: border-box;
+        width: 100%;
+        max-width: 760px;      
+        border-radius: 14px;
+        border: 1px solid rgba(225, 225, 225, 0.2);
+        background-color: rgb(250, 250, 250);
+        display: flex;
+        flex-direction: column;
+        -webkit-box-align: center;
+        align-items: center;
+        overflow: hidden;
+    }
+`;
+
+const Div = styled.div `
+    div {
+        font-size: 20px;
+    line-height: 1.4;
+    color: rgb(0, 0, 0);
+    font-weight: 700;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-bottom: 12px;
+    }
+`;
+
+
+const Div2 = styled.div `
+    div {
+        white-space: pre-line;
+        font-size: 14px;
+        line-height: 1.86;
+        color: rgb(62, 82, 101);
+        margin-bottom: auto;
+        text-align: center;
+        display: block;
+    }
+`;
+
+
+const Div3 = styled.div `
+    div {
+        opacity: 1;
+        transform: translateY(0px);
+        display: flex;
+        align-items: flex-start;
+    }
+`;
+
+const InDiv = styled.div `
+    div {
+        background-color: rgb(12, 36, 59);
+    border-radius: 8px;
+    padding: 6px;
+    margin-right: 8px;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    font-size: 20px;
+    line-height: 1.4;
+    color: rgb(0, 0, 0);
+    font-weight: 700;
+    }
+`;
+
+const InDiv3 = styled.div `
+    div {
+        width: 100%;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        animation-duration: 16s;
+        animation-name: cSrSTz;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+        transform: translateX(0px);
+        transform: translateX(-1140px);
+    }
+`;
+
+const Icons = styled.div `
+    div {
+        display: inline-flex;
+        line-height: 1;
+        font-size: 18px;
+        font-family: 'Material Icons';
+    font-weight: normal;
+    font-style: normal;
+    letter-spacing: normal;
+    text-transform: none;
+    white-space: nowrap;
+    direction: ltr;
+    -webkit-font-smoothing: antialiased;
+    color: rgb(255, 255, 255);
+    -webkit-box-align: center;
+    align-items: center;
+    }
+`;
+
+const Indiv32 = styled.div `
+    div {
+        width: 100%;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        animation-duration: 16s;
+        animation-name: cSrSTz;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+        transform: translateX(0px);
+        transform: translateX(-1140px);
+    }
+`;
+
+const ImgAll = styled.div `
+    img {
+        margin-right: 16px;
+        width: 212px;
+        object-fit: contain;
+    font-family: "object-fit: contain;";
+    }
+`;
+
 const Middle2 = () => {
     return (
         <div>
-
+            <Middle2Section>
+                <section>
+                    <Middle2Area>
+                        <div>
+                            <H2>
+                                <h2>투명하고 직관적인 인사정보</h2>
+                            </H2>
+                            <H3>
+                            <h3>여기저기 흩어진 인사기록, 엑셀 파일과 이제 작별 인사할 시간입니다.</h3>
+                            </H3>
+                        </div>
+                    </Middle2Area>
+                    <Middle2Area2>    
+                        <div>
+                            <Ul>
+                                <ul>
+                                    <Li>
+                                        <li>
+                                            <Button>
+                                                <button>리포트</button>
+                                            </Button>
+                                        </li>
+                                    </Li>
+                                    <Li23>
+                                        <li>
+                                            <Button23>
+                                                <button>프로필</button>
+                                            </Button23>
+                                        </li>
+                                        <li>
+                                            <Button23>
+                                                <button>문서관리</button>
+                                            </Button23>
+                                        </li>
+                                    </Li23>
+                                </ul>
+                            </Ul>
+                            <ImgArea>
+                                <div>
+                                    <Div>
+                                        <div>
+                                            <InDiv>
+                                                <div>
+                                                    <Icons>
+                                                    리포트<div className="meterial-icons">work</div>
+                                                    </Icons>
+                                                </div>
+                                            </InDiv>
+                                        </div>
+                                    </Div>
+                                    <Div2>
+                                        <div>근무시간, 연차, 인사정보, 계약정보 등
+                                        인사기록 리포트를 내려받아 보세요.</div>
+                                    </Div2>
+                                    <Div3>    
+                                        <div>
+                                            <InDiv3>
+                                                <div>
+                                                    <Indiv32>
+                                                        <div>
+                                                            <ImgAll>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                                <img alt="구성원 리포트 스크린샷" src=""/>
+                                                            </ImgAll>
+                                                        </div>
+                                                    </Indiv32>
+                                                </div>
+                                            </InDiv3>
+                                        </div>
+                                    </Div3>
+                                </div>
+                            </ImgArea>
+                        </div>
+                    </Middle2Area2>    
+                </section>
+            </Middle2Section>
         </div>
     )
 }
